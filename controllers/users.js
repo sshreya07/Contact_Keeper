9c14487bed4745505158c1e99870a87c3dfd56ed
Shreya Gupta <gshreya0799@gmail.com>
2021-11-09 17:59:24 +0530

    #3 updated

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,29 @@
+const User = require('../models/User');
+const { validationResult } = require('express-validator');
+
 //@route    POST api/users
 //@desc     register a user
 //@access   Public
 exports.createUser = async (req, res, next) => {
 
     // console.log(req.body);
+    const errors = validationResult(req);
 
+    if(!errors.isEmpty()){
+        res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body; 
+    
+    //create user
+    const user = await User.create({
+        name, 
+        email,
+        password,
+    });     //create data in our database
+    
     res.status(201).json({
-        msg: `user ${req.body.name} has been registered`
-    });
-
-    next();
+        success: true,
+        data: user
+    })
 }
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name:{
@@ -14,7 +15,8 @@ const UserSchema = new mongoose.Schema({
 
     password: {
         type: String,
-        required: [true, 'Please add a password']
+        required: [true, 'Please add a password'],
+        select:false
     },
 
     createdAt: {
@@ -22,5 +24,21 @@ const UserSchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+
+//encrypting password using bcryptjs
+UserSchema.pre('save', async function (next) {
+    //isModified('field') method
+    if(!this.isModified('password')){
+      next();
+    }
+    
+    const salt = await bcrypt.genSalt(10);    // 10 is the no of rounds to make strong encryption (but remember the more the no of rounds the more stronger but heavier your app will be )
+
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+  });
+
 
 module.exports = mongoose.model('user', UserSchema);
